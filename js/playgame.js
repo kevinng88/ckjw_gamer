9bd865566b31364eef948b427cf660bd3d34f9db
CHE-WEI HSU <jimmyhsu39@gmail.com>
2017-08-28 10:18:49 +0800

    Jimmy

--- js/playgame.js
+++ js/playgame.js
@@ -7,24 +7,25 @@ const OXYGEN_CONSUME_RATE = '';
 
 
 class PlayGame{
-    
+
 
     create(){
         this.firefighter = "";      //sprite: our player in the game
-        this.smallpig = "";         //sprite: the small-size pig - have less energy to fire burnt, will consume small amount of oxygen when picked by fireman 
+        this.smallpig = "";         //sprite: the small-size pig - have less energy to fire burnt, will consume small amount of oxygen when picked by fireman
         this.bigpig = "";           //sprite: the big-size pig - have more energy to fire burnt, will consume more amount of oxygen when picked by fireman
         this.s_fire = "";           //sprite: the random fire on the map
         this.b_fire = "";           //sprite: the big screen width fire on the bottom. Will going up on screen when time pass
         this.water = "";            //sprite: the water spread from firefighter
         this.oxygen = "";           //integer: level of oxygen consumed by firefighter
-        this.score_s_pig = "";      //integer: number of small-size pig collected by firefighter 
-        this.score_b_pig = "";      //integer: number of big-size pig collected by firefighter 
+        this.score_s_pig = "";      //integer: number of small-size pig collected by firefighter
+        this.score_b_pig = "";      //integer: number of big-size pig collected by firefighter
         //////////additional variables go here/////////////
 
 
         /////////////////Ching's section////////////////////////////
 
         //ching's code here
+        kokokokoksoko
 
         ////////////////////////////////////////////////////////////
 
