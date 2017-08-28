97c18149bf4a6fbc8d3171f210e1161195cb6e01
CHE-WEI HSU <jimmyhsu39@gmail.com>
2017-08-28 14:11:28 +0800

    sound

--- js/playgame.js
+++ js/playgame.js
@@ -25,7 +25,7 @@ class PlayGame{
         /////////////////Ching's section////////////////////////////
 
         //ching's code here
-        kokokokoksoko
+
 
         ////////////////////////////////////////////////////////////
 
@@ -33,8 +33,12 @@ class PlayGame{
 
 
         /////////////////Jimmy's section////////////////////////////
-
-        //jimmy's code here
+        game.load.audio("bgmusic", ["assets/sounds/bgmusic.mp3"]);
+        this.bgMusic = game.add.audio("bgmusic");
+        this.bgMusic.loopFull(1);
+        this.bgMusic.stop();
+	      var explosionSound = game.add.audio("explosion");
+	      explosionSound.play();
 
         ////////////////////////////////////////////////////////////
 
