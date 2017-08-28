aaa01bfb972bb06b62181f481b7a0ecb27fbaf6c
WatsonFungHK <watson.cwfung@gmail.com>
2017-08-28 09:54:15 +0800

    game states completed

--- js/game.js
+++ js/game.js
@@ -3,13 +3,14 @@ let game;
 window.onload = function(){
 
     game = new Phaser.Game(640, 960);
-    
+
     game.state.add("Boot", Boot);
     game.state.add("Preload", Preload);
-    //game.state.add("Titlescreen", TitleScreen);
-    game.state.add("Playgame", PlayGame);     
-    //game.state.add("GameOverScreen", Gameoverscreen);
-
+    game.state.add("Titlescreen", TitleScreen);
+    game.state.add("Playgame", PlayGame);
+    game.state.add("GameOverScreen", Gameoverscreen);
+    game.state.add("WinGame", WinGame);
+    game.state.start("Boot", Boot);
 
 
 }
@@ -18,11 +19,11 @@ window.onload = function(){
 class Boot{
     preload(){
         //this.game.load.image("loading", "assets/sprites/loading.png");
-        
+
     }
 
     create(){
-        
+
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
