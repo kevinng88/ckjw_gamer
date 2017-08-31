b265a5887d50b2cb9d19563d407cb7a3b25104ce
WatsonFungHK <watson.cwfung@gmail.com>
2017-08-31 17:56:40 +0800

    instruction spacing

--- js/titlescreen.js
+++ js/titlescreen.js
@@ -5,10 +5,10 @@ class TitleScreen {
       //  Jimmy's section: background image
           var barn = game.add.image(30, 20, "barn");
           barn.maxWidth = game.world.width - 200;
-          var storyText = "Go save poor piglets from a barn on fire. The farmer would be thankful to your brave deed!"
-          var storyline = game.add.bitmapText(60, game.world.height / 2, 'carrier_command', storyText, 20)
+          var storyText = "Go save poor piglets from\r\ra barn on fire. \r\rThe farmer would be thankful \r\rfor your brave deed!"
+          var storyline = game.add.bitmapText(60, game.world.height / 2, 'carrier_command', storyText, 17)
           storyline.maxWidth = game.world.width - 60;
-          var bmpText= game.add.bitmapText(50, game.world.height - 280, 'carrier_command', 'How to play!', 30);
+          var bmpText= game.add.bitmapText(90, game.world.height - 280, 'carrier_command', 'How to play!', 30);
       //  Jimmy's section: playbutton
           var playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
           playButton.anchor.set(0.5);
