b382312358a30d6e6a1f496817c6e63c0c0cdc95
kevinng88 <gstcollection@hotmail.com>
2017-08-31 10:51:33 +0800

    kevin: temp

--- js/playgame.js
+++ js/playgame.js
@@ -322,9 +322,9 @@ class PlayGame{
         // this.bigpig.body.velocity.x = 0;
         // this.bigpig.body.velocity.y = 0;
 
-
+        
         game.physics.arcade.overlap(this.firefighter, this.smallpig, function(fighter, pig){
-
+            
             //this function will kill 1 pig, then reset in another position, return the number of pig
             this.score_s_pig = pig_regeneration(pig, this.score_s_pig, this.show_score);
             var gettingpigSound = game.add.audio("gettingpig");
@@ -334,15 +334,18 @@ class PlayGame{
         game.physics.arcade.overlap(this.smallpig, this.s_fire, function(pig, fire){
            //console.log("燒豬肉: " + this.smallpig.getIndex(pig) + "火: " + this.s_fire.getIndex(fire));
             pig_burn(pig);
+           
             // console.log( this.pigss_alive.children[this.smallpig.getIndex(pig)].width - 0.1);
                 if(PIG_HEALTH - PIG_HIT_FIRE_HURT < 0){
                         this.smallpig.children[this.smallpig.getIndex(pig)].kill();
                 }
             //console.log( this.pigss_alive.children[this.smallpig.getIndex(pig)].width - 0.1);
+           
                 if(this.pigss_alive.children[this.smallpig.getIndex(pig)].width - PIG_HIT_FIRE_HURT < 0){
                         pig.kill();
                         this.pigss_alive.children[this.smallpig.getIndex(pig)].kill();
                         this.pigss_BG.children[this.smallpig.getIndex(pig)].kill();
+                        
                         pig_regeneration(pig, this.smallpig, this.pigss_BG, this.pigss_alive);
                         console.log("PIG DIED DUE TO FIRE");
                 } else if(this.pigss_alive.children[this.smallpig.getIndex(pig)].width >= 0){
@@ -530,11 +533,12 @@ class PlayGame{
 
 function pig_burn(pig){
     pig.animations.play("burn", 10, true);
+    pig.anchor.setTo(0.5,1);
     game.add.tween(pig).from({tint : Math.random() * 0xffffff}, 1000, Phaser.Easing.Linear.None, true).chain(
         game.add.tween(pig).to({ tint : 0xffffff }, 10, "Linear", true)  // can't test out??
     ) ;
-    game.add.tween(pig).to({y: pig.y - 50}, 500, Phaser.Easing.Circular.Out, true)
-    game.add.tween(pig).to({y: pig.y + 50}, 1000, Phaser.Easing.Bounce.Out, true,500);
+    game.add.tween(pig).to({y: pig.y - 10}, 500, Phaser.Easing.Circular.Out, true)
+    game.add.tween(pig).to({y: pig.y + 10}, 1000, Phaser.Easing.Bounce.Out, true,500);
     //game.add.tween(pig).to({alpha: 0.5}, 1000, Phaser.Easing.Bounce.Out, true,2, true);
 
 }
@@ -605,14 +609,16 @@ function pig_kill(pig, pig_grp, score, text, red_bar, green_bar){
 
     red_bar.children[pig_grp.getIndex(pig)].kill();
     green_bar.children[pig_grp.getIndex(pig)].kill();
-    pig_regeneration(pig, pig_grp, /*score, text,*/ red_bar, green_bar);
+    pig_regeneration(pig, pig_grp, score, text, red_bar, green_bar);
     return score;
 }
 
-function pig_regeneration(pig, pig_grp, /*score, text,*/ red_bar, green_bar){
+function pig_regeneration(pig, pig_grp, score, text, red_bar, green_bar){
     //regenerate the pig again.....
     //////////////////////REGENERATE INTERVAL IS 1s to 7s)
     //for animation start (
+
+    
     var t = game.rnd.integerInRange(1000, 7000);
     // console.log(t);
     game.time.events.add(t, function () {
