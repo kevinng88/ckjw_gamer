b330f03d03f862c299da1a8e51436ca2d39c7da9
WatsonFungHK <watson.cwfung@gmail.com>
2017-09-01 13:17:12 +0800

    merge

--- js/playgame.js
+++ js/playgame.js
@@ -51,7 +51,7 @@ class PlayGame{
         this.background.alpha = 0.9;
 
         game.physics.startSystem(Phaser.Physics.ARCADE); // T1
-        this.firefighter = game.add.sprite(60, 100, 'fighter');      //sprite: our player in the game
+        this.firefighter = game.add.sprite(6 * grid, 10.5 * grid, 'fighter');      //sprite: our player in the game
         this.smallpig = game.add.group();          //sprite: the small-size pig - have less energy to fire burnt, will consume small amount of oxygen when picked by fireman
         this.bigpig = game.add.group();//game.add.sprite(100, 100, 's_pigv');  //[[test]]          //sprite: the big-size pig - have more energy to fire burnt, will consume more amount of oxygen when picked by fireman
         this.s_fire = game.add.group();          //sprite: the random fire on the map
