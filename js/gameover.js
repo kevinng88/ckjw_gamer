5393a70efc942d93b1dd81fcc119eba7de8092b5
WatsonFungHK <watson.cwfung@gmail.com>
2017-09-01 13:01:28 +0800

    merge

--- js/gameover.js
+++ js/gameover.js
@@ -1,33 +1,41 @@
 class GameOverScreen {
-        create(){
-            console.log("enter lose game screen");
-            game.stage.backgroundColor = "#434343";
-            thisGameTimeLeft=timeLeft;
-            // pig_kill(pig, this.smallpig, this.score_s_pig, this.show_score, this.pigss_alive, this.pigss_BG);
-            caughtNumber=0;
-            PIG_DIED_DUE_TO_FIRE = 0;
-            OXYGEN_NOW=OXYGEN_STARTING_VOLUMN;
-            var image1= game.add.image(game.width/2-200, game.height/2-100 ,'sausage');
-            image1.anchor.set(0.5);
-            var image2= game.add.image(game.width/2-200, game.height/2 +100,'flax');
-            image2.anchor.set(0.5);
-            var youLoseText= game.add.bitmapText(game.world.width/2+10, game.world.height/2-300, 'carrier_command', 'YOU LOSE !', 50);
-            youLoseText.anchor.set(0.5);
-            // var bmpText1= game.add.bitmapText(game.width / 2, 50 , "font", "Small Pig Collected: ", 48).anchor.x = 0.5;
-            // var bmpText2= game.add.bitmapText(game.width / 2, 150 , "font", score.toString(), 72).anchor.x = 0.5;
-            const playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
-            playButton.anchor.set(0.5);
-            const tween = game.add.tween(playButton).to({
-            width: 220,
-            height:220
-            }, 1500, "Linear", true, 0, -1);
-            tween.yoyo(true);
-            }
+    create(){
+        console.log("enter lose game screen");
 
-          startGame(){
-            // this.myHealth.reset();
+        // style: images, background, color
+        game.stage.backgroundColor = "#434343";
+        var image1= game.add.image(game.width/2 + 100, 230,'sausage');
+        image1.anchor.set(0.5);
+        var image2= game.add.image(game.width/2 + 100, 430,'flax');
+        image2.anchor.set(0.5);
+        var font = 'carrier_command';
 
-            game.state.start("TitleScreen", true, false);
+        // winning statement
+        var sausageNumber = caughtNumber * 3;
+        var title = game.add.bitmapText(120, 100, font, "Good Job!", 40)
+        var sausageResult = game.add.bitmapText(game.width/2-100, 230, font, sausageNumber, 40);
+        var extinguishedBlaze = game.add.bitmapText(game.width/2-100, 430, font, fireScore, 50);
+        var losingDescription = `You die,\r\ralthough you saved ${this.score_s_pig} piglets.`;
+        var losingStory = game.add.bitmapText(60, 550, font, losingDescription, 18);
+        losingStory.maxWidth = 540;
 
-          }
-        }
+        //bottun
+        const playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
+        playButton.anchor.set(0.5);
+        const tween = game.add.tween(playButton).to({
+        width: 220,
+        height:220
+        }, 1500, "Linear", true, 0, -1);
+        tween.yoyo(true);
+
+        // reset game
+        caughtNumber=0;
+        OXYGEN_NOW=500;
+        thisGameTimeLeft = 180;
+    }
+
+    startGame(){
+        // this.myHealth.reset();
+        game.state.start("TitleScreen", true, false);
+    }
+}
